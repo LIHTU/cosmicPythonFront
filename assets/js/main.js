c9fd0578254e11228b3e1307663900154ee179af
Robin Nelson <robinanelson@gmail.com>
2016-04-21 15:34:16 -0700

    Yay!  Added variable nimbus sizes and offset the pulsing of the alpha layer into three different groups.

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,17 +41,45 @@ function canvasApp () {
 		context.font         = "72px Monospace";
 		context.textBaseline = "top";
 
-		if (fadeIn) {
-			alpha += 0.01;
-			if (alpha >= 1)  {
-				alpha = 1;
-				fadeIn = false;
+		if (fadeIn1) {
+			alpha1 += 0.01;
+			if (alpha1 >= 1)  {
+				alpha1 = 1;
+				fadeIn1 = false;
 			}
 		} else {
-			alpha -= 0.01;
-			if (alpha < .2)  {
-				alpha = .2;
-				fadeIn = true;
+			alpha1 -= 0.01;
+			if (alpha1 < .2)  {
+				alpha1 = .2;
+				fadeIn1 = true;
+			}
+		}
+
+		if (fadeIn2) {
+			alpha2 += 0.01;
+			if (alpha2 >= 1)  {
+				alpha2 = 1;
+				fadeIn2 = false;
+			}
+		} else {
+			alpha2 -= 0.01;
+			if (alpha2 < .2)  {
+				alpha2 = .2;
+				fadeIn2 = true;
+			}
+		}
+
+		if (fadeIn3) {
+			alpha3 += 0.01;
+			if (alpha3 >= 1)  {
+				alpha3 = 1;
+				fadeIn3 = false;
+			}
+		} else {
+			alpha3 -= 0.01;
+			if (alpha3 < .2)  {
+				alpha3 = .2;
+				fadeIn3 = true;
 			}
 		}
 
@@ -105,11 +133,23 @@ function canvasApp () {
 
 			for(i=0; i<coordsList.length; i++){
 				nextPoint = coordsList[i];
+				var nimbus = 5;
 
-				context.globalAlpha = alpha;
+				if (i==0 || i==3) {
+					context.globalAlpha = alpha1;
+					nimbus = 5;
+
+				} else if (i==1 || i==4) {
+					context.globalAlpha = alpha2;
+					nimbus = 7;
+				} else {
+					context.globalAlpha = alpha3;
+					nimbus = 9;
+				}
+
 				context.lineWidth = 1;
 				context.beginPath();
-				context.arc(nextPoint[0],nextPoint[1],5,0,2*Math.PI);
+				context.arc(nextPoint[0],nextPoint[1],nimbus,0,2*Math.PI);
 				context.stroke();
 				context.closePath();
 
@@ -178,8 +218,13 @@ function canvasApp () {
 	} // end of drawScreen
 
 	var text = "Pythonic Cosmos";
-	var alpha = .5;
-	var fadeIn = true;
+	var alpha1 = .5;
+	var alpha2 = .25;
+	var alpha3 = .9;
+	var fadeIn1 = true;
+	var fadeIn2 = true;
+	var fadeIn3 = true;
+
 
 	//image
 	var nebulaImg = new Image();
